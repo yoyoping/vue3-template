51055cafa980ca40f932f5be59ff6bdb7c3de12e
yoyoping <zhangping_ymd@outlook.com>
2022-02-25 18:20:10 +0800

    fix: 将fly替换成axios

--- src/fetch/index.ts
+++ src/fetch/index.ts
@@ -1,19 +1,13 @@
-/*
- * @Description: 请求封装
- * @Author: zhangping
- * @Date: 2019-07-19 17:40:49
- * @LastEditTime: 2019-09-24 17:37:29
- * @LastEditors: Please set LastEditors
- */
-import Fly, { FlyRequestConfig } from 'flyio'
-import ApiUri from './api'
-import { Throttle } from '../utils/index'
+import axios from 'axios'
 import { message } from 'ant-design-vue'
-import router from '../router/index'
+import ApiUri from './api'
 import store from '../store/app'
+import router from '../router/index'
+import { Throttle } from '../utils/index'
 
 // 节流
 const Throttle_: any = Throttle()
+
 // restful的参数
 interface IBindVars {
   key: string
@@ -27,71 +21,86 @@ export interface IParams {
   [key: string]: any
 }
 
-interface FlyRequestProps extends FlyRequestConfig {
-  params?: IParams
-}
-
-//添加请求拦截器
-Fly.interceptors.request.use((config: FlyRequestProps) => {
-  config.baseURL = process.env.NODE_ENV === 'production' ? '' : ''
-  // 处理参数,针对于绑定id的路由
-  if (config.body && Array.isArray(config.body.bindVars)) {
-    config.body.bindVars.forEach((item: IBindVars) => {
-      if (!!config.url) {
-        config.url = config.url.replace(`:${item.key}`, `${item.value}`)
-      }
-    })
-    delete config.body.bindVars
-  }
-  config.headers['Content-Type'] = 'application/json'
-  config.headers['Access-Control-Allow-Origin'] = '*'
-  config.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
-  config.headers['Authorization'] = sessionStorage.getItem('token') || ''
-  config.timeout = 30000
-  // 判断是否需要展示加载组件
-  if (config.body._showLoading) {
-    config.headers['_showLoading'] = true
-    delete config.body._showLoading
-    store().updateGlobalLoading(true)
-  }
-  return config
+// 配置的环境变量
+const VITE_BASE_API: any = import.meta.env.VITE_BASE_API
+// create an axios instance
+export const request = axios.create({
+  baseURL: VITE_BASE_API,
+  timeout: 30000
 })
 
-//添加响应拦截器，响应拦截器会在then/catch处理之前执行
-Fly.interceptors.response.use(
-  (response: any) => {
-    // 判断当前请求是否是需要显示加载组件，如果是就取消
-    if (response.request.headers._showLoading) {
-      store().updateGlobalLoading(false)
+// request interceptor
+request.interceptors.request.use(
+  config => {
+    if (config.data && Array.isArray(config.data.bindVars)) {
+      config.data.bindVars.forEach((item: any) => {
+        if (config.url) {
+          config.url = config.url.replace(`:${item.key}`, `${item.value}`)
+        }
+      })
+      delete config.data.bindVars
     }
-    let { data, status } = response
+
+    // 添加header
+    if (config.headers) {
+      config.headers['Content-Type'] = 'application/json'
+      config.headers['Access-Control-Allow-Origin'] = '*'
+      config.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+      config.headers['X-Token'] = sessionStorage.getItem('token') || ''
+
+      // 判断是否需要展示加载组件
+      // if (config.data._showLoading) {
+      //   config.headers['_showLoading'] = true
+      //   delete config.data._showLoading
+      //   store().updateGlobalLoading(true)
+      // }
+    }
+
+    return config
+  },
+  error => {
+    return Promise.reject(error)
+  }
+)
+
+request.interceptors.response.use(
+  (response: any) => {
+    let { data, status, config } = response
     if (status >= 200 && status < 300 && data.code === 0) {
+      const oldApi = ['/api/v1/getCaptcha', '/login']
+      if (oldApi.includes(response.request.url)) {
+        return data
+      }
       return data.data
+    } else if (data.code === 401) {
+      // 登录状态过期
+      message.error('登录状态已过期，请重新登录！')
+      sessionStorage.clear()
+      router.push({ name: 'login' })
+      return Promise.reject(response)
+    } else if (status >= 200 && status < 300) {
+      // 判断如果是下载文件流的接口直接返回
+      return data
     } else {
       message.error(data.msg || '系统错误！')
-      return new Promise((response, reject) => {
-        reject(response)
-      })
+      return Promise.reject(response)
     }
   },
-  (error: any) => {
+  error => {
     // 判断当前请求是否是需要显示加载组件，如果是就取消
-    if (error.request.headers._showLoading) {
+    if (error.response.headers._showLoading) {
       store().updateGlobalLoading(false)
     }
     let errDescription = '服务异常，请重试！'
-    switch (error.status) {
+    switch (error.response.status) {
       case 400:
-        errDescription = error?.data?.detail || '参数错误！'
+        errDescription = error?.data?.detail || '请求参数错误！'
         break
       case 401:
-        errDescription = '授权失败'
+        errDescription = '登录状态已过期，请重新登录！'
         break
       case 404:
-        errDescription = '资源未找到！'
-        break
-      case 500:
-        errDescription = '服务异常，请重试！'
+        errDescription = '请求资源未找到！'
         break
       default:
         errDescription = '服务异常，请重试！'
@@ -100,45 +109,41 @@ Fly.interceptors.response.use(
       message.error(errDescription)
     }, 300)
     // token失效，鉴权失败
-    if (error.status === 401) {
+    if (error.response.status === 401) {
       sessionStorage.clear()
       router.push({ name: 'login' })
       return
     }
-    return new Promise((response, reject) => {
-      reject(error)
-    })
-    //发生网络错误后会走到这里
-    //promise.resolve("ssss")
+    return Promise.reject(error)
   }
 )
 
-let newApiUri: any = ApiUri
-const Fetch = (params: IParams) => {
+const newApiUri: any = ApiUri
+const fetch = (params: IParams) => {
   // 当前api对象
   const uriObj = newApiUri[params[`uriCode`]]
   // 请求的url
-  let uri = uriObj.restful ? uriObj.uri + '/' + params['id'] : uriObj.uri
+  const url = uriObj.restful ? uriObj.uri + '/' + params['id'] : uriObj.uri
   // 请求的方法类型
-  let method_: 'get' | 'post' | 'patch' | 'delete' | 'put'
+  let method
   if (!uriObj.method) {
-    method_ = 'get'
+    method = 'get'
   } else {
-    method_ = uriObj.method
+    method = uriObj.method
   }
   // 获取传给后端的参数
-  let param = JSON.parse(JSON.stringify(params))
-  delete param[`uriCode`]
-  delete param.method
+  const data = JSON.parse(JSON.stringify(params))
+  delete data[`uriCode`]
+  delete data.method
   if (uriObj.restful) {
-    delete param.id
+    delete data.id
   }
-  // 把是否展示加载组件添加到请求的参数里面，方便判断
-  if (uriObj.showLoading) {
-    param._showLoading = true
+  const _service: any = request
+  if (['get', 'delete'].includes(method)) {
+    return _service[method](url, { params: data })
+  } else {
+    return _service[method](url, data)
   }
-  const _fly: any = Fly
-  return _fly[method_](uri, param)
 }
 
-export default Fetch
+export default fetch
